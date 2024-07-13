2710b2470424df455dbef632a6a6c4267ae27bd3
Gwendal Roué <gr@pierlis.com>
2024-07-13 17:42:36 +0200

    [BREAKING] Add dependency on GRDB

--- Package.swift
+++ Package.swift
@@ -20,13 +20,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GRDBQuery",
-            dependencies: []),
+            dependencies: [
+                .product(name: "GRDB", package: "GRDB.swift"),
+            ]),
     ]
 )
 
